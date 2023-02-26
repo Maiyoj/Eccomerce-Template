ea3880df61815a96ccfb53475568f18adfa21582
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-26 20:10:42 +0300

    add style to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,26 @@
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
+import {BsPerson} from "react-icons/bs"
+import {TbShoppingCartPlus} from "react-icons/tb"
 const  Navbar = ()=>{
     return(
         <Fragment>
-            <nav>
+            <nav className="flex justify-evenly ">
                 <Link to={'/'}  exact="true">ShopCart</Link>
                 <Link to={'/'}  exact="true">Categories</Link>
                 <Link to={'/'}  exact="true">Deals</Link>
                 <Link to={'/'}  exact="true">Whats New</Link>
                 <Link to={'/'}  exact="true">Delivery</Link>
+                <input className="bg-gray-200"/>
+                <Link to={'/'}  exact="true">
+                <div className="flex justify-evenly ">
+                    <BsPerson className="  fontSize: rem"/> Account
+                </div>
+                </Link> 
+                <Link>
+                <div className="flex justify-evenly ">
+                <TbShoppingCartPlus/> Cart
+                </div></Link>
             </nav>
         </Fragment>
     )
