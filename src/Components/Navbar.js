a0a435ec1877af0ab72ce70b7dc4e20c41373fbb
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-27 21:51:58 +0300

    update navabar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,21 +5,21 @@ import {TbShoppingCartPlus} from "react-icons/tb"
 const  Navbar = ()=>{
     return(
         <Fragment>
-            <nav className="flex justify-evenly ">
+            <nav className="flex justify-evenly mt-7">
                 <Link to={'/'}  exact="true">ShopCart</Link>
                 <Link to={'/'}  exact="true">Categories</Link>
                 <Link to={'/'}  exact="true">Deals</Link>
                 <Link to={'/'}  exact="true">Whats New</Link>
                 <Link to={'/'}  exact="true">Delivery</Link>
-                <input className="bg-gray-200"/>
+                <input className="bg-gray-200 rounded-full"/>
                 <Link to={'/'}  exact="true">
-                <div className="flex justify-evenly ">
-                    <BsPerson className="  fontSize: rem"/> Account
+                <div className="flex justify-between">
+                    <BsPerson className="m-1"/> Account
                 </div>
                 </Link> 
                 <Link>
-                <div className="flex justify-evenly ">
-                <TbShoppingCartPlus/> Cart
+                <div className="flex justify-around ">
+                <TbShoppingCartPlus className="m-1"/> Cart
                 </div></Link>
             </nav>
         </Fragment>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 const Home = ()=>{
-    return <div>Welcome Home</div>
+    return <div></div>
 }
 
 export default Home;
