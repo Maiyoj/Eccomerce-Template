b2a6ef780e3e716adaed0d6b2ed65e80ecb1a6be
Maiyoj <valentinemaiyo1995@gmail.com>
2023-05-09 23:30:23 +0300

    update home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,8 +4,14 @@ const Home = ()=>{
     return (
     <Fragment>
     <div className="">
-    <div></div>
-    <div></div>
+    <div>
+        <p>Shopping And </p>
+        <p>Department Store.</p>
+        <p>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
+    </div>
+    <div>
+        <img src="../Images/Home.png"/>
+    </div>
     </div>
     </Fragment>
     )
