af35ebc95db3d84f5327cf89aff650fb8d47046e
Maiyoj <valentinemaiyo1995@gmail.com>
2023-05-10 22:42:27 +0300

    update home image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,17 @@
 import { Fragment } from "react";
 
 const Home = ()=>{
+    const image = require('../Images/download.jpg');
     return (
     <Fragment>
+     <div style={{
+      backgroundImage: `url(${image})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      height: '70vh'
+    }}>
+      <h1>My App</h1>
+    </div>
     <div className="">
     <div>
         <p>Shopping And </p>
@@ -10,7 +19,6 @@ const Home = ()=>{
         <p>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
     </div>
     <div>
-        <img src="../Images/Home.png"/>
     </div>
     </div>
     </Fragment>
