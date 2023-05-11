6d1f837021bb80b47321462d93becc436a0f3824
Maiyoj <valentinemaiyo1995@gmail.com>
2023-05-11 23:16:28 +0300

    fix home page image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,17 @@
 import { Fragment } from "react";
 
 const Home = ()=>{
-    const image = require('../Images/Home.png');
+    const image = require('../Images/bg-stage.png');
     return (
     <Fragment>
-     <div style={{
-      backgroundImage: `url(${image})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      height: '70vh'
-    }}>
-      <h1>My App</h1>
-    </div>
-    <div className="">
-    <div>
-        <p>Shopping And </p>
-        <p>Department Store.</p>
-        <p>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
-    </div>
-    <div>
-    <img src={`url(${image})`} alt="This is an image" />
-    </div>
-    </div>
+      <div
+      className="bg-cover bg-center flex items-center">
+       <img src={image} alt="Example" className="h-96" />
+       </div>
+      <div className="ml-10">
+        <h1 className="text-3xl font-bold">Shopping And Department Store.</h1>
+        <p className="text-lg">Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
+      </div>
     </Fragment>
     )
 }
